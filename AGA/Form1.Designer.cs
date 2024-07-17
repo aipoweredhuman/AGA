2fa7cae7062c80f3e34e0cf0e306c8d2a4f6f34d
iseedeadpeop1e <125872837+iseedeadpeop1e@users.noreply.github.com>
2024-07-18 02:34:20 +1000

    Интерфейс + заполнение данными

--- AGA/Form1.Designer.cs
+++ AGA/Form1.Designer.cs
@@ -28,21 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.save_button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // save_button
+            // 
+            this.save_button.Location = new System.Drawing.Point(1287, 697);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(172, 56);
+            this.save_button.TabIndex = 1;
+            this.save_button.Text = "Сохранить";
+            this.save_button.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1447, 572);
+            this.dataGridView1.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1471, 755);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.save_button);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- AGA/Form1.cs
+++ AGA/Form1.cs
@@ -1,45 +1,163 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AGA
 {
     public partial class Form1 : Form
     {
+        private List<Task> tasks; // Список всех задач, загруженных из API
+
+        private TextBox directionTextBox;
+        private TextBox responsibleTextBox;
+        private TextBox stageTextBox;
+        private TextBox deadlineTextBox;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeFilterControls();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void InitializeFilterControls()
         {
-            {
-                Dictionary<string, string> Params = new Dictionary<string, string>()
-    {
-        { "", "" },
-        { "", "" },
-        { "", "" },
-        { "", "" },
-    };
-               var answer = GetRequest("https://khakasia.bitrix24.ru/rest/1870/5l6u5im92ao6r42m/tasks.task.list?filter%5BGROUP_ID%5D=832&start=50./", Params).Result;
-                Console.WriteLine(answer.ToString());
-                Console.ReadLine();
-            }
+            // Создание текстовых полей для фильтров с подписями
+            TableLayoutPanel filterPanel = new TableLayoutPanel();
+            filterPanel.RowCount = 1;
+            filterPanel.ColumnCount = 8;
+            filterPanel.Dock = DockStyle.Top;
+            filterPanel.AutoSize = true;
+            this.Controls.Add(filterPanel);
+
+            filterPanel.Controls.Add(new Label { Text = "Направление:", AutoSize = true }, 0, 0);
+            directionTextBox = new TextBox();
+            directionTextBox.TextChanged += ApplyFilters;
+            directionTextBox.Tag = "Direction"; // Тег для идентификации колонки
+            filterPanel.Controls.Add(directionTextBox, 1, 0);
+
+            filterPanel.Controls.Add(new Label { Text = "Ответственный:", AutoSize = true }, 2, 0);
+            responsibleTextBox = new TextBox();
+            responsibleTextBox.TextChanged += ApplyFilters;
+            responsibleTextBox.Tag = "Responsible"; // Тег для идентификации колонки
+            filterPanel.Controls.Add(responsibleTextBox, 3, 0);
+
+            filterPanel.Controls.Add(new Label { Text = "Стадия:", AutoSize = true }, 4, 0);
+            stageTextBox = new TextBox();
+            stageTextBox.TextChanged += ApplyFilters;
+            stageTextBox.Tag = "Stage"; // Тег для идентификации колонки
+            filterPanel.Controls.Add(stageTextBox, 5, 0);
+
+            filterPanel.Controls.Add(new Label { Text = "Срок:", AutoSize = true }, 6, 0);
+            deadlineTextBox = new TextBox();
+            deadlineTextBox.TextChanged += ApplyFilters;
+            deadlineTextBox.Tag = "Deadline"; // Тег для идентификации колонки
+            filterPanel.Controls.Add(deadlineTextBox, 7, 0);
         }
-        static async Task<HttpResponseMessage> GetRequest(string adress, Dictionary<string, string> Params)
+
+        private async void Form1_Load(object sender, EventArgs e)
         {
-            HttpClient client = new HttpClient();
-            Uri uri = new Uri(adress);
-            var content = new FormUrlEncodedContent(Params);
-            return await client.PostAsync(adress, content);
+            Console.WriteLine("Получаем задачи из сервиса...");
+            string url = "https://khakasia.bitrix24.ru/rest/1870/5l6u5im92ao6r42m/tasks.task.list?filter[GROUP_ID]=832";
+            TaskFetcher taskFetcher = new TaskFetcher();
+            tasks = await taskFetcher.FetchAllTasksAsync(url);
+
+            dataGridView1.DataSource = tasks;
+        }
+
+        private void ApplyFilters(object sender, EventArgs e)
+        {
+            List<Task> filteredTasks = tasks;
+
+            if (!string.IsNullOrEmpty(directionTextBox.Text))
+            {
+                filteredTasks = filteredTasks.Where(t => t.Direction.IndexOf(directionTextBox.Text, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(responsibleTextBox.Text))
+            {
+                filteredTasks = filteredTasks.Where(t => t.Responsible.IndexOf(responsibleTextBox.Text, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(stageTextBox.Text))
+            {
+                filteredTasks = filteredTasks.Where(t => t.Stage.IndexOf(stageTextBox.Text, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(deadlineTextBox.Text))
+            {
+                filteredTasks = filteredTasks.Where(t => t.Deadline.IndexOf(deadlineTextBox.Text, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+            }
+
+            dataGridView1.DataSource = filteredTasks;
+        }
+    }
+
+    public class Task
+    {
+        public string Direction { get; set; }
+        public string Consumer { get; set; }
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Priority { get; set; }
+        public string Responsible { get; set; }
+        public string Stage { get; set; }
+        public string Deadline { get; set; }
+    }
+
+    public class TaskFetcher
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<List<Task>> FetchAllTasksAsync(string initialUrl)
+        {
+            List<Task> allTasks = new List<Task>();
+            string url = initialUrl;
+            int start = 0;
+
+            while (true)
+            {
+                HttpResponseMessage response = await client.GetAsync($"{url}&start={start}");
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(responseBody);
+                JArray tasksArray = (JArray)json["result"]["tasks"];
+
+                foreach (var item in tasksArray)
+                {
+                    Task task = new Task
+                    {
+                        Direction = "Улучшение", // Пример значения, можно изменить по необходимости
+                        Consumer = "1 ОСНОВНЫЕ ПРОЦЕССЫ", // Пример значения, можно изменить по необходимости
+                        Id = (string)item["id"],
+                        Project = (string)item["title"],
+                        Priority = (string)item["priority"],
+                        Responsible = (string)item["responsible"]["name"],
+                        Stage = "Готово", // Пример значения, можно изменить по необходимости
+                        Deadline = "" // Пример значения, можно изменить по необходимости
+                    };
+
+                    allTasks.Add(task);
+                }
+
+                if (json["next"] == null)
+                {
+                    break;
+                }
+
+                int next = (int)json["next"];
+                if (next == start || tasksArray.Count == 0)
+                {
+                    break;
+                }
+                start = next;
+            }
+
+            return allTasks;
         }
     }
 }
